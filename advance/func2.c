ba68c6a95cc6e4dd1d3f9ee0faac500ef64647e0
olatunjhi <kinghorlatunjhi.com>
2023-11-10 19:55:38 +0100

    updated

---
+++ advance/func2.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+void exeCommand(char *command)
+{
+	char *parsing[60];
+	int i = 0;
+	char *token = strtok(command, " \n");
+
+	if (token != NULL)
+	{
+		while (token != NULL)
+		{
+			parsing[i++] = token;
+			token = strtok(NULL, " \n");
+		}
+		parsing[i] = NULL;
+
+		if (strcasecmp(parsing[0], "exit") == 0)
+		{
+			exit(0);
+		}
+
+		pid_t pid = fork();
+
+		if (pid < 0)
+		{
+			perror("fork");
+		}
+		else if (pid == 0)
+		{
+			if (execve(parsing[0], parsing, NULL) == -1)
+			{
+				perror("execve");
+			}
+		}
+		else
+		{
+			int status;
+			wait(&status);
+		}
+	}
+}
---
+++ advance/shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void exeCommand(char *command);
+void printUserMessage(char *shellProgram);
+void print(const char *command);
+
+#endif
